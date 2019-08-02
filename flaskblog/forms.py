50bb58d179e1f93ecbfa9ab5fe0cceff9eee7678
Ghaffaru <mudashiruagm@gmail.com>
2019-08-02 00:21:40 +0000

    authentication

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField,BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 class RegisterationForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired(), Length(min=2,max=20) ])
@@ -10,8 +10,20 @@ class RegisterationForm(FlaskForm):
     confirm_password= PasswordField("Confirm password",validators=[DataRequired(),EqualTo("password")])
     submit = SubmitField("Sign up")
 
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken')
+
+    def validate_email(self,email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is already registered')
+
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     remember = BooleanField("Remember me")
     submit = SubmitField("Sign in")
+
+   
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,8 +1,8 @@
-from flask import Flask,render_template,url_for,flash,redirect
+from flask import Flask,render_template,url_for,flash,redirect,request
 from flaskblog.forms import RegisterationForm, LoginForm
-from flaskblog import app
+from flaskblog import app, db, bcrypt
 from flaskblog.models import User,Post
-
+from flask_login import login_user,current_user,logout_user,login_required
 posts = [
     {
         "author": "Coreh Shcaeer",
@@ -31,19 +31,37 @@ def login():
     form = LoginForm()
 
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash("Login unsuccessful, please check username and password", 'danger')
+            user = User.query.filter_by(email=form.email.data).first()
+
+            if user and bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user,remember=form.remember.data)
+                next_page = request.args.get('next')
+                return redirect(next_page) if next_page else redirect(url_for('home'))
+            else:
+                flash("Login unsuccessful, please check username and password", 'danger')
 
     return render_template('login.html', title='login', form=form)
 
 @app.route('/register',methods=['GET','POST'])
 def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegisterationForm()
     if form.validate_on_submit():
-        flash('Account created for ' + form.username.data,'success')
+        password_hashed = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=password_hashed)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has now been created, you can now login ')
         return redirect(url_for('home'))
     return render_template('signupp.html', title='register',form=form)
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html',title="Account")
